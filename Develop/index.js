e82dc5b912d4e9a9fca50ee9cdd76c08f421de91
Jaiar Rose <lfj143@gmail.com>
2021-07-17 16:36:48 -0700

    Finished Project

--- Develop/index.js
+++ Develop/index.js
@@ -1,77 +1,43 @@
 const generateHTML = (answers) =>
   `
 # ${answers.projectTitle}
+
+Link: ${answers.linkToProj}
+
 ## Description
 ${answers.description}
-Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
-- What was your motivation?
-- Why did you build this project? (Note: the answer is not "Because it was a homework assignment.")
-- What problem does it solve?
-- What did you learn?
-## Table of Contents (Optional)
-If your README is long, add a table of contents to make it easy for users to find what they need.
+
+## Table of Contents
+
 - [Installation](#installation)
 - [Usage](#usage)
 - [Credits](#credits)
 - [License](#license)
+
 ## Installation
 ${answers.installationInst}
-What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
+
 ## Usage
 ${answers.usageInst}
-Provide instructions and examples for use. Include screenshots as needed.
-To add a screenshot, create an "assets/images" folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:
-    md
-    ![alt text](assets/images/screenshot.png)
-  
+
 ## Credits
 ${answers.ghUsername}- Github
+${answers.email}- Email
 
-List your collaborators, if any, with links to their GitHub profiles.
-If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
-If you followed tutorials, include links to those here as well.
 ## License
-![License](https://img.shields.io/badge/license-${answers.license}-blue) //
-The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
----
-🏆 The previous sections are the bare minimum, and your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
-## Badges
-![badmath](https://img.shields.io/github/languages/top/nielsenjared/badmath)
-Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
+![License](https://img.shields.io/badge/license-${answers.license}-blue) 
+
 ## Features
-If your project has a lot of features, list them here.
+${answers.features}
+
 ## How to Contribute
 ${answers.contributionInst}
-If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
+See [Contributor Covenant](https://www.contributor-covenant.org/)
+
 ## Tests
 ${answers.testingInst}
 Go the extra mile and write tests for your application. Then provide examples on how to run them here.
 `;
-// </li>
-// </ul>
-// </div>
-// </div>
-// </body>
-// </html>
-// <!DOCTYPE html>
-// <html lang="en">
-// <head>
-//   <meta charset="UTF-8">
-//   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-//   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-//   <title>Document</title>
-// </head>
-// <body>
-//   <div class="jumbotron jumbotron-fluid">
-//   <div class="container">
-//     <h1 class="display-4">Hi! My name is ${answers.projectTitle}</h1>
-//     <p class="lead">I got squeezed out in ${answers.description}.</p>
-//     <p class="lead">I get giggles from${answers.installationInst}.</p>
-//     <p class="lead">I enjoy putting inside me, ${answers.usageInst}.</p>
-//     <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
-//     <ul class="list-group">
-//       <li class="list-group-item">You can find my half done projects on GitHub with my username, which is ${answers.ghUsername}</li>
-//       <li class="list-group-item">Me don have a job, here's a link to why:
 
 const inquirer = require("inquirer");
 const fs = require("fs");
@@ -97,24 +63,24 @@ inquirer
     {
       type: "input",
       name: "usageInst",
-      message: "What you like to put in you?",
-      choices: [
-        "children's soul",
-        "children's tears",
-        "Jack's quiche",
-        "after-dinner mints",
-      ],
+      message: "What is the intended usage of this product?",
+    },
+    {
+      type: "list",
+      name: "license",
+      message: "Licensing options",
+      choices: ["MIT", "BSD3"],
     },
     {
       type: "input",
       name: "contributionInst",
       message:
-        "Whats your github user name, I want to see a bunch of half done projects",
+        "How will any other developer make contributions?",
     },
     {
       type: "input",
       name: "testingInst",
-      message: "show me where I can see why you don't have a job",
+      message: "How would you like the user to test functionality?(Be as detailed as possible)",
     },
     {
       type: "input",
@@ -127,10 +93,14 @@ inquirer
       message: "Email: ",
     },
     {
-      type: "list",
-      name: "license",
-      message: "show me where I can see why you don't have a job",
-      choices: ["MIT", "BSD3"],
+      type: "input",
+      name: "linkToProj",
+      message: "Project Link:",
+    },
+    {
+      type: "input",
+      name: "features",
+      message: "features included: ",
     },
   ])
   .then((answers) => {
--- README(Actual).md
+++ README(Actual).md
@@ -1,2 +1,38 @@
-# Readme-Only
-Readme Generator For developers to make a clear and concise readme's for their projects
+
+# ReadMe Only
+
+Link: https://github.com/MagnetMoss/Readme-Only
+
+## Description
+Read me generator created for the puropse of having a template for which future developers can create a readme for their prject with ease.
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+See the credits section to find me on github, you can fork the repository from there and use at your own leisure
+
+## Usage
+use to create a template for a readme to be put out. You can also modify the code and make it into a completely new document.
+
+## Credits
+MagnetMoss- Github
+2xF4x0@gmail.com- Email
+
+## License
+![License](https://img.shields.io/badge/license-MIT-blue) 
+
+## Features
+doing great things.
+
+## How to Contribute
+see credits to find content creator on github and fork the repository.
+See [Contributor Covenant](https://www.contributor-covenant.org/)
+
+## Tests
+Open in terminal using code editor of choice and running node in the terminal
+Go the extra mile and write tests for your application. Then provide examples on how to run them here.
